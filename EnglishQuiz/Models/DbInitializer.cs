e8b5f26e502baf8fa1ca91a8a8075381257f650b
Chris Duehrsen <duehrsen@hotmail.com>
2015-01-11 20:58:45 -0800

    Added seed data (some britam, some japanese)

--- EnglishQuiz/Models/DbInitializer.cs
+++ EnglishQuiz/Models/DbInitializer.cs
@@ -12,48 +12,86 @@ namespace EnglishQuiz.Models
         {
             base.Seed(context);
 
-            var questions = new List<Question>();
-
-            questions.Add(new Question { 
-            GameId= (int) Game.Britam,
-            Choice1="trolley",
-            Choice2="cart",
-            Image="cart.jpg"
-            });
-
-            questions.Add(new Question
+            var questions = new List<Question>
             {
-                GameId = (int) Game.Britam,
-                Choice1 = "lift",
-                Choice2 = "elevator",
-                Image = "elevator.jpg"
-            });
-
-            questions.Add(new Question
-            {
-                GameId = (int)Game.Britam,
-                Choice1 = "chemist",
-                Choice2 = "pharmacist",
-                Image = "pharmacist.jpg"
-            });
-
-            questions.Add(new Question
-            {
-                GameId = (int) Game.Japanese,
-                Choice1 = "skinship",
-                Choice2 = "affection",
-                Image = ""
-            });
-
-            questions.Add(new Question
-            {
-                GameId = (int) Game.Japanese,
-                Choice1 = "renewal",
-                Choice2 = "renovated",
-                Image = ""
-            });
-
-
+                new Question
+                {
+                    GameId = (int) Game.Britam,
+                    Choice1 = "trolley",
+                    Choice2 = "cart",
+                    Image = "cart.jpg"
+                },
+                new Question
+                {
+                    GameId = (int) Game.Britam,
+                    Choice1 = "lift",
+                    Choice2 = "elevator",
+                    Image = "elevator.jpg"
+                },
+                new Question
+                {
+                    GameId = (int) Game.Britam,
+                    Choice1 = "chemist",
+                    Choice2 = "pharmacist",
+                    Image = "pharmacist.jpg"
+                },
+                new Question
+                {
+                    GameId = (int) Game.Japanese,
+                    Choice1 = "skinship",
+                    Choice2 = "affection",
+                    Image = "gokusen.jpg"
+                },
+                new Question
+                {
+                    GameId = (int) Game.Japanese,
+                    Choice1 = "renewal",
+                    Choice2 = "renovated",
+                    Image = "gokusen.jpg"
+                },
+                new Question
+                {
+                    GameId = (int) Game.Britam,
+                    Choice1 = "underground",
+                    Choice2 = "subway",
+                    Image = "subway.jpg"
+                },
+                new Question
+                {
+                    GameId = (int) Game.Britam,
+                    Choice1 = "bonnet",
+                    Choice2 = "hood",
+                    Image = "hood.jpg"
+                },
+                new Question
+                {
+                    GameId = (int) Game.Britam,
+                    Choice1 = "city centre",
+                    Choice2 = "downtown",
+                    Image = "downtown.jpg"
+                },
+                new Question
+                {
+                    GameId = (int) Game.Britam,
+                    Choice1 = "car park",
+                    Choice2 = "parking lot",
+                    Image = "parkinglot.jpg"
+                },
+                new Question
+                {
+                    GameId = (int) Game.Britam,
+                    Choice1 = "petrol station",
+                    Choice2 = "gas station",
+                    Image = "gasstation.jpg"
+                },
+                new Question
+                {
+                    GameId = (int) Game.Britam,
+                    Choice1 = "windscreen",
+                    Choice2 = "windshield",
+                    Image = "windshield.jpg"
+                }
+            };
 
             questions.ForEach(q => context.Questions.Add(q));
             context.SaveChanges();
